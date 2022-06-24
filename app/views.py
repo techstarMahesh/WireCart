a4952b8859f713c0c85b0523a02a95e215d00abd
Mahesh <mistermaheshsharma@gmail.com>
2022-06-24 22:46:17 +0530

    "Class Created"

--- app/views.py
+++ app/views.py
@@ -1,46 +1,46 @@
 from django.shortcuts import render
-from django.shortcuts import View
-from .models import *
+from django.views import View
+from .models import Customer, Product, Cart, OrderPlaced
 
 # def home(request):
 #  return render(request, 'app/home.html')
 
 class ProductView(View):
     def get(self, request):
-        topwears = Prroduct.objects.filter(category='TW')
-        buttomwears = Prroduct.objects.filter(category='BW')
-        mobiles = Prroduct.objects.filter(category='M')
-        return render(request, 'app/home.html', {'topwears': topwears, 'buttomwears': buttomwears, 'mobiles': mobiles})
+        topwears = Product.objects.filter(category='TW')
+        bottomwears = Product.objects.filter(category='BW')
+        mobiles = Product.objects.filter(category='M')
+        return render(request, 'app/home.html', {'topwears': topwears, 'bottomwears': bottomwears, 'mobiles': mobiles})
 
 def product_detail(request):
- return render(request, 'app/productdetail.html')
+    return render(request, 'app/productdetail.html')
 
 def add_to_cart(request):
- return render(request, 'app/addtocart.html')
+    return render(request, 'app/addtocart.html')
 
 def buy_now(request):
- return render(request, 'app/buynow.html')
+    return render(request, 'app/buynow.html')
 
 def profile(request):
- return render(request, 'app/profile.html')
+    return render(request, 'app/profile.html')
 
 def address(request):
- return render(request, 'app/address.html')
+    return render(request, 'app/address.html')
 
 def orders(request):
- return render(request, 'app/orders.html')
+    return render(request, 'app/orders.html')
 
 def change_password(request):
- return render(request, 'app/changepassword.html')
+    return render(request, 'app/changepassword.html')
 
 def mobile(request):
- return render(request, 'app/mobile.html')
+    return render(request, 'app/mobile.html')
 
 def login(request):
- return render(request, 'app/login.html')
+    return render(request, 'app/login.html')
 
 def customerregistration(request):
- return render(request, 'app/customerregistration.html')
+    return render(request, 'app/customerregistration.html')
 
 def checkout(request):
- return render(request, 'app/checkout.html')
+    return render(request, 'app/checkout.html')
